8dbf27881e96d4db21fbc281912880af1f7c78d9
Dockedchutoy <116732014+Dockedchutoy@users.noreply.github.com>
2022-11-05 19:41:33 +0100

    Better code and bugfix
    1. The code is updated and much more readable.
    2. Filemode Interpreter now works perfectly, and the debug print() is gone

--- Asterisk_Launcher.py
+++ Asterisk_Launcher.py
@@ -13,47 +13,40 @@ try:
     file = sys.argv[1]
     if file.endswith(".ast"):
         filemode = True
+    else:
+        raise IndexError
 except IndexError:
     pass
 
 #Manual Interpreter
 
-if filemode == False:
-    while True:
-        cmd = input("*> ")
-        if cmd == "*":
-            print("Hello World")
-        elif cmd == " * ":
-            print(random() * 2147483647)
-        elif cmd == "*+*":
-            while True:
-                pass
 
-        elif cmd.startswith("run"):
+while True:
+    cmd = input("*> ")
+    if cmd == "*":
+        print("Hello World")
+    elif cmd == " * ":
+        print(random() * 2147483647)
+    elif cmd == "*+*":
+        while True:
+            pass
+
+    #File Mode Interpreter
+
+    elif cmd.startswith("run") or filemode == True:
+        if filemode == False:
             cmd, file = cmd.split(" ", 1)
-            with open(file) as f:
-                for cmd in f:
-                    print(cmd)
-                    cmd = cmd.replace("\n", " ")
-                    if cmd == "*":
-                        print("Hello World")
-                    elif cmd == " * ":
-                        print(random() * 2147483647)
-                    elif cmd == "*+*":
-                        while True:
-                            pass
 
-#File Mode Interpreter
+        with open(file) as f:
+            for cmd in f:
+                cmd = cmd.replace("\n", "")
+                if cmd == "*":
+                    print("Hello World")
+                elif cmd == " * ":
+                    print(random() * 2147483647)
+                elif cmd == "*+*":
+                    while True:
+                        pass
 
-
-if filemode == True:
-    with open(file) as f:
-        for cmd in f:
-            print(cmd)
-            if cmd == "*":
-                print("Hello World")
-            elif cmd == " * ":
-                print(random() * 2147483647)
-            elif cmd == "*+*":
-                while True:
-                    pass
+        if filemode == True:
+            break
